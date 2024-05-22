0aef057c5383338d33fafe4c27c0af51b90ab564
Pablo Cosio <pablo.cosio@validatedid.com>
2024-05-22 23:12:18 +0200

    build: remove files from src

--- track-and-trace-wrapper/src/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONFIG_OPTS = void 0;
-exports.CONFIG_OPTS = {
-    pilot: {
-        TntApiUrl: 'https://api-pilot.ebsi.eu/SISTEMARE!!!!!',
-        domain: 'https://api-pilot.ebsi.eu',
-    },
-};
