d9411ebc47b0810544d61976c2ba874ba75eea23
davide Icert <davide.porro@infocert.it>
2024-05-27 10:00:17 +0200

    fixed wrong signature-wrapper version in TnTwrapper's package.json

--- launchOnAllComponentsFolder.js
+++ launchOnAllComponentsFolder.js
@@ -2,20 +2,37 @@ const { promisify } = require('util');
 const { exec } = require('child_process');
 const execAsync = promisify(exec);
 
-const folderArray = require( "./allComponentsFolder") ;
+const folderArray = require("./allComponentsFolder");
 
 
 const sequentialExecution = async (...commands) => {
-    if (commands.length === 0) {
-        return 0;
+
+    try {
+
+
+        if (commands.length === 0) {
+            return 0;
+        }
+        console.log("execution of command: " + commands[0]);
+        const { stderr, err, stdout } = await execAsync(commands.shift());
+        if (stderr) {
+
+            console.log(stderr);
+            throw stderr;
+        }
+        if (err) {
+
+            console.log(err);
+            throw err;
+        }
+        console.log(stdout);
+
+        return sequentialExecution(...commands);
     }
-    console.log( "execution of command: " + commands[0]);
-    const { stderr, stdout } = await execAsync(commands.shift());
-    if (stderr) {
-        throw stderr;
+    catch (error) {
+
+        return;
     }
-    console.log( stdout );
-    return sequentialExecution(...commands);
 }
 
 // Checks for --command and if it has a value
@@ -35,20 +52,20 @@ if (commandValue.trim().length <= 0) {
 }
 
 let commands = [];
-folderArray.forEach( function( folder, index  ){
+folderArray.forEach(function (folder, index) {
 
-        commands[index] = "npm " + commandValue + " --prefix ./" + folder;
+    commands[index] = "npm " + commandValue + " --prefix ./" + folder;
 
 })
 
-sequentialExecution( ...commands);
+sequentialExecution(...commands);
 
- /*   // Will execute the commands in series
-    sequentialExecution(
-        "npm run " + commandValue + " --prefix ./signature-wrapper",
-        "npm run " + commandValue + " --prefix ./authorisation-wrapper",
-        "npm run " + commandValue + " --prefix ./track-and-trace-wrapper"
-    ); */
+/*   // Will execute the commands in series
+   sequentialExecution(
+       "npm run " + commandValue + " --prefix ./signature-wrapper",
+       "npm run " + commandValue + " --prefix ./authorisation-wrapper",
+       "npm run " + commandValue + " --prefix ./track-and-trace-wrapper"
+   ); */
 
 
 
