54ee88510e9e9c9c4d29dda5bfd8d5d0273e3f47
davide Icert <davide.porro@infocert.it>
2024-05-28 08:12:23 +0200

    modifiche a interfaccia I t&t

--- track-and-trace-wrapper/src/interfaces/TnTWrapper.interface.ts
+++ track-and-trace-wrapper/src/interfaces/TnTWrapper.interface.ts
@@ -3,19 +3,22 @@ import { Document } from '../types/document';
 import { Optional } from '../types/optional';
 import { DocumentData } from '../types/types';
 
-/** Interface TnTWrapper */
+/** 
+ Interface TnTWrapper  
+ main responsibility: hiding the ethereum details exposed by the api
+*/
 export interface ITnTWrapper {
-  /**
+  /** create a new document in the ledger ( it manage also the signing of the transaction )
    * @param documentHash base64 encoded document hash
-   * @param documentMetadata document metadata
+   * @param documentMetadata base64 encoded document metadata
    */
   createDocument(
     documentHash: string,
     documentMetadata: string,
   ): Promise<string>;
-  addEventToDocument(): any;
-  getDocument(documentHash: string): Promise<DocumentData>;
-  getEvent(): any;
   listDocuments(): any;
+  getDocument(documentHash: string): Promise<DocumentData>;
+  addEventToDocument(): any;
   listEventOfDocument(): any;
+  getEvent(): any;
 }
