8495bc4e01902cbaffc4bcddec187bd02728ba8e
mtodescato <matteo.todescato@infocert.it>
2024-06-12 17:46:57 +0200

    Update Result requing Error extension and add Optional

---
+++ error-wrapper/src/types/result.ts
@@ -0,0 +1,46 @@
+export class Result<T, E extends Error> {
+  value: T | null;
+  error: E | null;
+
+  private constructor(value: T | null, error: E | null) {
+    this.value = value;
+    this.error = error;
+  }
+
+  public static ok<T, E extends Error>(value: T): Result<T, E> {
+    return new Result(value, null);
+  }
+
+  public static err<T, E extends Error>(error: E): Result<T, E> {
+    return new Result(null, error);
+  }
+
+  public isOk(): boolean {
+    return this.value !== null;
+  }
+
+  public isErr(): boolean {
+    return this.error !== null;
+  }
+
+  public unwrap(): T {
+    if (this.value === null) {
+      throw new Error('No value present');
+    }
+    return this.value;
+  }
+
+  public unwrapErr(): E {
+    if (this.error === null) {
+      throw new Error('No error present');
+    }
+    return this.error;
+  }
+
+  public unwrapOr(other: T): T {
+    if (this.value === null) {
+      return other;
+    }
+    return this.value;
+  }
+}
